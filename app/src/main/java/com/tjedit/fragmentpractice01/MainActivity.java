d84d0e01f5274814e7b2b9f1860910708b829501
user <kyronko@naver.com>
2019-05-11 10:20:59 +0900

    Initial commit

---
+++ app/src/main/java/com/tjedit/fragmentpractice01/MainActivity.java
@@ -0,0 +1,37 @@
+package com.tjedit.fragmentpractice01;
+
+import android.databinding.DataBindingUtil;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tjedit.fragmentpractice01.databinding.ActivityMainBinding;
+
+public class MainActivity extends BaseActivity {
+
+    ActivityMainBinding act;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bindViews();
+        setupEvents();
+        setupValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setupValues() {
+
+    }
+
+    @Override
+    public void bindViews() {
+
+            act = DataBindingUtil.setContentView(this,R.layout.activity_main);
+    }
+}
